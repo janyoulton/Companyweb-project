f7dc3b0abef6f26a681462790ded648610242f40
Jan Youlton <jan.youlton@thecustomerlink.be>
2022-11-10 10:52:58 +0100

    Added in HTML message as toast messages are not showing to display result of add list of companies callout method.

--- companyweb/force-app/main/default/lwc/companyWebAddCompanyList_LWC/companyWebAddCompanyList_LWC.js
+++ companyweb/force-app/main/default/lwc/companyWebAddCompanyList_LWC/companyWebAddCompanyList_LWC.js
@@ -1,23 +1,28 @@
-import {LightningElement, api} from 'lwc';
+import {LightningElement, api, track} from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import addCompanyListToAlerts from '@salesforce/apex/CompanyWebController.addCompanyListToAlerts';
 
-export default class ListViewButton extends LightningElement {
+export default class ListViewButton extends LightningElement {    
 	@api listViewIds;
+    @track bodyMessage;
 
     onAddListOfCompanies(){
         console.log('onAddListOfCompanies method');
         if(this.listViewIds){
             addCompanyListToAlerts({ 'listViewIds': this.listViewIds})
             .then(result => {
+                this.bodyMessage = result ? 'Companies successfully added to Companyweb Alerts' : 'Companies could not be added to Companyweb Alerts';
+                console.log(result ? 'Companies successfully added to Companyweb Alerts' : 'Companies could not be added to Companyweb Alerts');
                 this.showNotification('Add list of Companies to Alerts', result ? 'Companies successfully added to Companyweb Alerts' : 'Companies could not be added to Companyweb Alerts', result ? 'success' : 'error');
             })
             .catch(error => {
                 window.console.log('Error Occured', error);
                 if(Object.values(error.body.fieldErrors).length > 0)
                 {
+                    this.bodyMessage = Object.values(error.body.fieldErrors)[0][0].message;
                     this.showNotification('Oops', Object.values(error.body.fieldErrors)[0][0].message, 'error');
                 } else if (error.body.pageErrors.length > 0){
+                    this.bodyMessage = error.body.pageErrors[0].message;
                     this.showNotification('Oops', error.body.pageErrors[0].message, 'error');
                 }
             })
@@ -25,6 +30,8 @@ export default class ListViewButton extends LightningElement {
                 this.close();
             });
         } else{
+            this.bodyMessage = 'Select at least one company to be added to Alerts';
+            console.log('Select at least one company to be added to Alerts');
             this.showNotification('Warning','Select at least one company to be added to Alerts', 'warning');
         }
 
@@ -46,7 +53,7 @@ export default class ListViewButton extends LightningElement {
 			function() {
 				window.history.back();
 			},
-			1000
+			2000
 		);
 	}
 }
